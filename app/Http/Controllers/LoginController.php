cb88fc57f0d475e9c889c96a04d22fca98c6667a
Alantec2000 <alansantana1995@hotmail.com>
2020-09-13 21:15:25 -0300

    login - Agrupadas as rotas no namespace dos controllers. Criada a primeira versão do código de autenticacao do usuário.

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,10 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
 {
     public function Login() {
+        $params = request()->all();
+
+        $validation = Validator::make($params, [
+            'email' => 'required|string',
+            'senha' => 'required|string'
+        ]);
+
+        if ($validation->fails()) {
+            $responseContent = [
+                'status' => 'error',
+                'data' => $validation->errors()
+            ];
+            return response($responseContent, 403);
+        }
+
+        $authenticationInfo = [
+            'email' => $params['email'],
+            'password' => $params['senha']
+        ];
+
+        if (!Auth::attempt($authenticationInfo)) {
+            abort(403, "E-mail ou senha inválido!");
+        }
+
         return view('login.index');
     }
 
