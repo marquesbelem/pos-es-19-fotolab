f212d4449f2107694347dffe2163422ae38499f4
marquesbelem <camilambdasilva@gmail.com>
2020-12-03 18:25:50 -0300

    Ajustando a tela do perfil do fotografo

--- public/js/main.js
+++ public/js/main.js
@@ -5,9 +5,42 @@ $('.carousel').slick({
     fade: true,
     cssEase: 'linear',
     autoplay: true,
-    arrows: false
+    arrows: false,
+    focusOnSelect: false
   });
 
 $('.error-alert').click(function (e) {
   $(this).remove();
+});
+
+$('.carousel-center').slick({
+  centerMode: true,
+  centerPadding: '10px',
+  slidesToShow: 3,
+  dots: true,
+  autoplay: true,
+  arrows: false,
+  focusOnSelect: false,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '10px',
+        slidesToShow: 1,
+        focusOnSelect: false
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '10px',
+        slidesToShow: 1,
+        focusOnSelect: false
+      }
+    }
+  ]
 });
